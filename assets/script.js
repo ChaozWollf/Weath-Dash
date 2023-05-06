8edb29e5a5118df7a60b4487ec66d3f407038931
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-05-06 18:09:21 -0500

    trying to get the fecth and localStorage to work correctly

--- assets/script.js
+++ assets/script.js
@@ -14,17 +14,44 @@ const APIstorm = "46b8f30c47e39abae3cbe5ebea90da76"
 const city = $('.city-name');
 var cit= $('#city');
 const sity=JSON.parse(localStorage.getItem('Cname'));
+const sh = $('.SH');
+const ff= $('.ff')
+
+function weather(){
+const requestUrl = 'https://api.openweathermap.org / data / 2.5 / weather ? q = { cit } & APPID==={APIstorm}';
+
+
+fetch(requestUrl)
+.then(function (response){
+    return response.json();
+})
+.then(function(data) {
+    for(let i = 0; i <data.length; i++) {
+        const createTableRow=document.createElement('tr');
+        const tableData = document.createElement('td');
+        const link = document.createElement('a');
+        link.textContent = data[i].html_url;
+        link.href = data[i]/html_url;
+        tableData.appendChild(link);
+        createTableRow.appendChild(tableData)
+        ff.appendChild(createTableRow)
+    }
+});
+
+}
+
+
+
 
 
     const timer = window.setInterval(function() {
-        const Ctime = dayjs().format('MMMM:D:HH:mm')
-        var Date = $("<p></p>").text(Ctime.Val());
-        $(".navbar-brand").append(Date);
-        
-    }, 60000);
+        const Ctime = dayjs().format('MMMM,D')
+        var Date = $("<p></p>").text(Ctime);
+        $("Nava").text(Date[0].outerText);
+    
+    }, 1000);
 
-
-console.log(timer)
+$(".Nava").append(Date);
 
 
 
@@ -36,5 +63,5 @@ $('.button').on('click', function() {
     const neu = cit
     cit.push(neu)
     localStorage.setItem('Cname', JSON.stringify(cit.val()));
-
+    weather
 });
