c9a742313d5e5e0d094cf7351fa641977f47c822
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-05-07 12:13:20 -0500

    trying to get fetch to work

--- assets/script.js
+++ assets/script.js
@@ -12,65 +12,64 @@
 
 const APIstorm = "46b8f30c47e39abae3cbe5ebea90da76"
 const city = $('.city-name');
-var cit= $('#city');
-const sity=JSON.parse(localStorage.getItem('Cname'));
+var cit = $('#city');
+const sity = JSON.parse(localStorage.getItem('Cname'));
 const sh = $('.SH');
-const ff= $('.ff')
+const ff = $('.ff')
 const uity = [];
 
 
 
 
+https://api.openweathermap.org/data/2.5/weather?q=Toronto,CA&appid={yourkey}
+
+
+
+function weather() {
+    const requestUrl = 'https://api.openweathermap.org/data/2.5/weather?q={cit.val()}&appid={44d550e17ada64b6e8f690bbafcd026a}'
+
+    // 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={46b8f30c47e39abae3cbe5ebea90da76}'
+
+    // Parameters
+
+
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+
+            const cityName = data.name;
+            const temp = data.main.temp;
+            const desc = data.weather[0].description;
+            const createTableRow = document.createElement('tr');
+            const cityNameData = document.createElement('td');
+            cityNameData.textContent = cityName;
+            createTableRow.appendChild(cityNameData);
+            const tempData = document.createElement('td');
+            tempData.textContent = temp;
+            createTableRow.appendChild(tempData);
+            const descData = document.createElement('td');
+            descData.textContent = desc;
+            createTableRow.appendChild(descData);
+            ff.appendChild(createTableRow);
+
+
+
+        });
+}
 
 
 
 
 
 
+const timer = window.setInterval(function () {
+    const Ctime = dayjs().format('MMMM,D')
+    var Date = $("<p></p>").text(Ctime);
+    $("Nava").text(Date[0].outerText);
 
-
-
-function weather(){
-    const requestUrl = "https://api.openweathermap.org/data/2.5/weather?q={cit.val()}&APPID==={APIstorm}";
-
-
-fetch(requestUrl)
-.then(function (response){
-    return response.json();
-})
-.then(function(data) {
-    
-    const cityName = data.name;
-    const temp = data.main.temp;
-    const desc = data.weather[0].description;
-    const createTableRow = document.createElement('tr');
-    const cityNameData = document.createElement('td');
-    cityNameData.textContent = cityName;
-    createTableRow.appendChild(cityNameData);
-    const tempData = document.createElement('td');
-    tempData.textContent = temp;
-    createTableRow.appendChild(tempData);
-    const descData = document.createElement('td');
-    descData.textContent = desc;
-    createTableRow.appendChild(descData);
-    ff.appendChild(createTableRow);
-    
-    
-    
-});
-}      
-    
-
-
-
-
-
-    const timer = window.setInterval(function() {
-        const Ctime = dayjs().format('MMMM,D')
-        var Date = $("<p></p>").text(Ctime);
-        $("Nava").text(Date[0].outerText);
-    
-    }, 1000);
+}, 1000);
 
 $(".Nava").append(Date);
 
@@ -80,12 +79,14 @@ $(".Nava").append(Date);
 
 
 
-$('.button').on('click', function() {
+$('.button').on('click', function () {
     uity.push(cit.val());
     localStorage.setItem('Cname', JSON.stringify(uity));
-for( i=0; i < uity.length; i++) {
+    sh.html("");
+    for (i = 0; i < uity.length; i++) {
         const bity = document.createElement("button");
         bity.textContent = uity[i];
-        bity.append(sh);
-   
-}});
+        sh.append(bity);
+        weather();
+    }
+});
