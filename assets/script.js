829fa77ca88a1256d38aac86527047e633eccd5b
ChaozWollf <128013056+ChaozWollf@users.noreply.github.com>
2023-05-09 23:07:28 -0500

    fixed image issue

--- assets/script.js
+++ assets/script.js
@@ -38,14 +38,14 @@ function weather() {
             console.log(data);
             const cityName = data.name;
             const createTableRow = document.createElement('tr');
-            const icon = data.weather.icon;
-            <img src = "https://openweathermap.org/img/wn/${icon}@2x.png"></img>;
+            const icon = data.weather[0].icon;
+            const img = document.createElement("img");
+            img.setAttribute("src", `https://openweathermap.org/img/wn/${icon}@2x.png`);
             const desc = data.weather[0].description;
             const temp = data.main.temp;
             const wind = data.wind.speed;
             const cityNameData = document.createElement('td');
             cityNameData.textContent = cityName;
-            console.log(icon)
             const descData = document.createElement('td');
             descData.textContent = desc;
             const tempData = document.createElement('tr');
@@ -73,7 +73,9 @@ function forcast() {
             return response.json();
         })
         .then(function (data) {
-            console.log(data);
+            const icon0 = data.list[0].weather[0].icon;
+            const img0 = document.createElement("img");
+            img0.setAttribute("src", `https://openweathermap.org/img/wn/${icon0}@2x.png`);
             const desc1 = data.list[0].weather[0].description;
             const temp1 = data.list[0].main.temp;
             const wind1 = data.list[0].wind.speed;
@@ -84,10 +86,14 @@ function forcast() {
             tempData1.textContent ="Temp: "+ temp1+ "F";
             const windData1 = document.createElement('tr');
             windData1.textContent = "Wind:"+ wind1 + " mph" ;
+            createTableRow1.appendChild(img0);
             createTableRow1.appendChild(descData1);
             createTableRow1.appendChild(tempData1);
             createTableRow1.appendChild(windData1);
             one[0].appendChild(createTableRow1);
+            const icon8 = data.list[8].weather[0].icon;
+            const img8 = document.createElement("img");
+            img8.setAttribute("src", `https://openweathermap.org/img/wn/${icon8}@2x.png`);
             const desc2 = data.list[8].weather[0].description;
             const temp2 = data.list[8].main.temp;
             const wind2 = data.list[8].wind.speed;
@@ -98,10 +104,14 @@ function forcast() {
             tempData2.textContent = "Temp:" + temp2 + " F";
             const windData2 = document.createElement('tr');
             windData2.textContent ="Wind:" + wind2 + " mph";
+            createTableRow2.appendChild(img8);
             createTableRow2.appendChild(descData2);
             createTableRow2.appendChild(tempData2);
             createTableRow2.appendChild(windData2);
             two[0].appendChild(createTableRow2);
+            const icon16 = data.list[16].weather[0].icon;
+            const img16 = document.createElement("img");
+            img16.setAttribute("src", `https://openweathermap.org/img/wn/${icon16}@2x.png`);
             const desc3 = data.list[16].weather[0].description;
             const temp3 = data.list[16].main.temp;
             const wind3 = data.list[16].wind.speed;
@@ -112,10 +122,14 @@ function forcast() {
             tempData3.textContent = "Temp:" + temp3 + " F";
             const windData3 = document.createElement('tr');
             windData3.textContent = "Wind:" + wind3 + " mph"; ;
+            createTableRow3.appendChild(img16);
             createTableRow3.appendChild(descData3);
             createTableRow3.appendChild(tempData3);
             createTableRow3.appendChild(windData3);
             three[0].appendChild(createTableRow3);
+            const icon24 = data.list[24].weather[0].icon;
+            const img24 = document.createElement("img");
+            img24.setAttribute("src", `https://openweathermap.org/img/wn/${icon24}@2x.png`);
             const desc4 = data.list[24].weather[0].description;
             const temp4 = data.list[24].main.temp;
             const wind4 = data.list[24].wind.speed;
@@ -126,10 +140,14 @@ function forcast() {
             tempData4.textContent = "Temp:" + temp4 + " F";
             const windData4 = document.createElement('tr');
             windData4.textContent = "Wind:" + wind4 + " mph";;
+            createTableRow4.appendChild(img24);
             createTableRow4.appendChild(descData4);
             createTableRow4.appendChild(tempData4);
             createTableRow4.appendChild(windData4);
             four[0].appendChild(createTableRow4);
+            const icon32 = data.list[32].weather[0].icon;
+            const img32 = document.createElement("img");
+            img32.setAttribute("src", `https://openweathermap.org/img/wn/${icon32}@2x.png`);
             const desc5 = data.list[32].weather[0].description;
             const temp5 = data.list[32].main.temp;
             const wind5 = data.list[32].wind.speed;
@@ -140,6 +158,7 @@ function forcast() {
             tempData5.textContent = "Temp:" + temp5 + " F" ;
             const windData5 = document.createElement('tr');
             windData5.textContent = "Wind:" + wind5 + " mph";
+            createTableRow5.appendChild(img32);
             createTableRow5.appendChild(descData5);
             createTableRow5.appendChild(tempData5);
             createTableRow5.appendChild(windData5);
